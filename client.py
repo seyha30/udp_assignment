6d7799bdb12aaf1d21d00cd7bd6d517829d09fb1
seyha30 <57175099+seyha30@users.noreply.github.com>
2022-06-12 15:28:50 +0700

    new commit

--- client.py
+++ client.py
@@ -1,9 +1,7 @@
-from dataclasses import field
 import socket
 import time
 client_socket = socket.socket(family=socket.AF_INET,type=socket.SOCK_DGRAM)
 file = open('log.txt','a')
-print(file)
 is_send = True
 counter = 1
 while is_send:
@@ -12,19 +10,17 @@ while is_send:
         string = input('Please input message :  ')
         client_socket.sendto(str.encode(string),('localhost',20001))
         start = time.time()
-        print('current time' ,start)
-        file.write(str(start))
-        file.write('\n')
-        file.flush()
-        
+        print('current time' ,start) 
     else:
         is_send = False
-
     print(client_socket.recvfrom(1024))
     end = time.time()
     elapsed = end - start
     print('Elapsed time ===  ', elapsed)
     counter += 1
+    file.write(str(elapsed))
+    file.write('\n')
+    file.flush()
 file.close()
     
     
