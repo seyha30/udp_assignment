9e06d41e4815c60d383f1f2f9ed936168d2cd7da
seyha30 <57175099+seyha30@users.noreply.github.com>
2022-06-12 14:49:05 +0700

    new

--- request.py
+++ request.py
@@ -1,15 +1,22 @@
+from dataclasses import field
 import socket
 import time
-client_socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+client_socket = socket.socket(family=socket.AF_INET,type=socket.SOCK_DGRAM)
+file = open('log.txt','a')
+print(file)
 is_send = True
 counter = 1
 while is_send:
     start = None
     if counter <= 6:
-        string = input('Please input string ==  ')
+        string = input('Please input message :  ')
         client_socket.sendto(str.encode(string),('localhost',20001))
         start = time.time()
         print('current time' ,start)
+        file.write(str(start))
+        file.write('\n')
+        file.flush()
+        
     else:
         is_send = False
 
@@ -18,5 +25,6 @@ while is_send:
     elapsed = end - start
     print('elapsed ===  ', elapsed)
     counter += 1
+file.close()
     
     
--- response.py
+++ response.py
@@ -5,18 +5,19 @@ bufferSize  = 1024
 msgFromServer       = "Hello UDP Client"
 bytesToSend         = str.encode(msgFromServer)
 # Create a datagram socket
-UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+server_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 # Bind to address and ip
-UDPServerSocket.bind((localIP, localPort))
+server_socket.bind((localIP, localPort))
 print("UDP server up and listening")
 # Listen for incoming datagrams
 while(True):
-    bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
+    bytesAddressPair = server_socket.recvfrom(bufferSize)
     message = bytesAddressPair[0]
     address = bytesAddressPair[1]
+    print(bytesAddressPair)
     clientMsg = "Message from Client:{}".format(message)
     clientIP  = "Client IP Address:{}".format(address)
     print(clientMsg)
     print(clientIP)
     # Sending a reply to client
-    UDPServerSocket.sendto(bytesToSend, address)
+    server_socket.sendto(bytesToSend, address)
